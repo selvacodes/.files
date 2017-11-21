b22db88dd66a667ab43e36bdf1c2a57056d5ce91
selvacodes <selvaganapathy@outlook.in>
2017-11-21 19:00:50 +0530

    Delete private.sh

--- .config/zsh_custom/private.sh
+++
@@ -1,5 +0,0 @@
-export VPN_HOST=https://myvpn.trstone.com
-export VPN_USER=sganapathy
-
-alias oc='sudo openconnect -u $VPN_USER -b -l --pid-file=$HOME/openconnect.pid $VPN_HOST --authgroup=RosettaStone'
-alias ocd='sudo kill `cat ~/openconnect.pid`'
