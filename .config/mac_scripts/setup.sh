48d7914041793d014e99df5cffbe730e0a7b02c5
Selva Ganapathy <selva.g@listertechnologies.com>
2018-04-24 00:11:58 +0530

    added chunkwm

--- .config/mac_scripts/setup.sh
+++ .config/mac_scripts/setup.sh
@@ -5,7 +5,19 @@ set -euo pipefail
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #echo "oh my zsh"
+##  Not needed ################################################
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+##################################################
+
+#echo "Install Zim"
+#git clone --recursive https://github.com/zimfw/zimfw.git ${ZDOTDIR:-${HOME}}/.zim
+
+#setopt EXTENDED_GLOB
+#for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
+  #user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
+  #touch ${user_file}
+  #( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
+#done
 
 #echo "Liquid prompt"
 #cd
